fd3e1f8e7f25d1f37a931e88a23c4ae77e32a864
unknown <farenabc123@gmail.com>
2024-08-22 08:33:46 +0700

    first commit

---
+++ Ex01/main.js
@@ -0,0 +1,4 @@
+D1_Marks_weight = 78
+D1_Marks_height = 1.69
+BMI = mass / (D1_Marks_height * D1_Marks_height)
+console.log(BMI)
---
+++ Ex02/main.js
@@ -0,0 +1,6 @@
+BMI_John = 13
+BMI_Marks = 12
+if (BMI_Marks > BMI_John) {
+    console.log("Mark's BMI is higher than John's")
+} else
+    console.log("John's BMI is higher than Mark's")
